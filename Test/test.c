cc8919c1ce58deda71e326ab9b60e6da6704fff2
Mellbero <rsstone2002@googlemail.com>
2023-11-30 16:39:31 +0100

    windows mag füße

--- Test/test.c
+++ Test/test.c
@@ -1,24 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/types.h> // Include the header for ssize_t
+#include <string.h>
 
-int main() {
-    FILE *file = fopen("input.txt", "r");
-    if (file == NULL) {
-        perror("Failed to open the file.\n");
-        return 1;
+int main(void) {
+    FILE *fp = fopen("input.txt", "r");
+    if(fp == NULL) {
+        perror("Unable to open file!");
+        exit(1);
     }
 
+    // Read lines using POSIX function getline
+    // This code won't work on Windows
     char *line = NULL;
-    size_t line_length = 0;
+    size_t len = 0;
 
-
-        while ((_getwline(&line, &line_length, file)) != -1) {
-            // Process the line here
-            printf("%s", line);
+    while(getline(&line, &len, fp) != -1) {
+        printf("line length: %zd\n", strlen(line));
     }
 
-    free(line);
-    fclose(file);
-    return 0;
+    printf("\n\nMax line size: %zd\n", len);
+
+    fclose(fp);
+    free(line);     // getline will resize the input buffer as necessary
+                    // the user needs to free the memory when not needed!
 }
